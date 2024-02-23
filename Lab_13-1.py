fead2cb27650cd0c271a4f06790c3df25e3f3974
mrconelius <144148053+mrconelius@users.noreply.github.com>
2024-02-22 20:49:33 -0500

    Create Lab_13-1.py

---
+++ Lab_13-1.py
@@ -0,0 +1,16 @@
+# Create a dictionary called row
+row = {
+  "computer1": "Matt",
+  "computer2": "Bob",
+  "computers": "Ben",
+  "computer4": "David"
+}
+
+# Print the dictionary
+print ("Original row:", row)
+
+# Someone else sat at computer1
+row["computer1"] = "Sarah"
+
+# Print the updated dictionary
+print("Updated row:", row)
