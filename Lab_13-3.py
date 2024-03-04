11443b54ed2652ecce4b6a013089ebadf8d51ee2
mrconelius <144148053+mrconelius@users.noreply.github.com>
2024-03-03 20:36:35 -0500

    Create Lab_13-3.py

---
+++ Lab_13-3.py
@@ -0,0 +1,9 @@
+# Define weekdays and weekends tuples
+weekdays = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
+weekends = ("Saturday", "Sunday")
+
+# Concatenate weekdays and weekends
+days_of_the_week = weekdays + weekends
+
+# Print the concatenated tuple
+print(days_of_the_week)
