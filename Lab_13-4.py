04120280a7f1aed56568ed66fcac00da47a55202
mrconelius <144148053+mrconelius@users.noreply.github.com>
2024-03-03 20:38:13 -0500

    Create Lab_13-4.py

---
+++ Lab_13-4.py
@@ -0,0 +1,12 @@
+# Create a list containing the number day for the next 5 days
+day_date = [2, 3, 4, 5, 6]
+
+# Assign the 5 letters from the list to variables representing each day
+thu, fri, mon, tue, wed = tuple(day_date)
+
+# Print each of the variables to see their values
+print("Thursday:", thu)
+print("Friday:", fri)
+print("Monday:", mon)
+print("Tuesday:", tue)
+print("Wednesday:", wed)
